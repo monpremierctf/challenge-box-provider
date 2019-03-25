0402f22c61cef993e4a61715e149811aed04cb90
Clément Cassé <casse.clement@outlook.com>
2019-03-25 16:27:19 +0100

    Adding docker command taht should go background. CleanDB now really cleans DB and is not a placeholder anymore. First stage of startup is now a cleaning of DB.

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 
 var (
 	challengeBoxDockerImage    string
+	challengeBoxDockerCmd      string
 	challengeBoxDockerPort     int
 	challengeBoxDockerLifespan int
 	httpServerListener         string
@@ -30,6 +31,7 @@ var (
 func init() {
 	// Set program flags
 	flag.StringVar(&challengeBoxDockerImage, "image", "ubuntu", "Docker image")
+	flag.StringVar(&challengeBoxDockerCmd, "cmd", "/usr/sbin/sshd", "Docker command to be send to background")
 	flag.IntVar(&challengeBoxDockerPort, "port", 22, "Container exposed port to dynamically map on the host")
 	flag.IntVar(&challengeBoxDockerLifespan, "life", 60, "Challenge lifetime before the box closes")
 	flag.StringVar(&httpServerListener, "listen", "0.0.0.0:8080", "Address:Port the http server will bind to")
@@ -61,13 +63,14 @@ func init() {
 
 }
 
-func createNewChallengeBox(box string, duration, port int) (containerID []byte, err error) {
+func createNewChallengeBox(box, cmd string, duration, port int) (containerID []byte, err error) {
 	containerIDDirty, err := exec.Command(
 		"docker", "container", "run",
 		"--detach", "--rm",
 		"--publish", fmt.Sprintf("%d", port),
 		fmt.Sprintf("%s", box),
-		"sleep", fmt.Sprintf("%d", duration)).Output()
+		"sh", "-c",
+		fmt.Sprintf("%s && sleep %d", cmd, duration)).Output()
 	containerID = bytes.TrimSpace(containerIDDirty)
 	return
 }
@@ -96,7 +99,7 @@ func provideChallengeBox(w http.ResponseWriter, r *http.Request) {
 
 		if containerID == nil {
 			log.Printf("Source IP %s is not known: creating a new challenge box.", srcIP)
-			boxID, err := createNewChallengeBox(challengeBoxDockerImage, challengeBoxDockerLifespan, challengeBoxDockerPort)
+			boxID, err := createNewChallengeBox(challengeBoxDockerImage, challengeBoxDockerCmd, challengeBoxDockerLifespan, challengeBoxDockerPort)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
@@ -129,36 +132,64 @@ func provideChallengeBox(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func cleanDB() {
-	containers, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{})
-	//containerSet := set.NewSetFromSlice(containers)
+func cleanDB() error {
+	db, err := bolt.Open("./state.db", 0600, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("Error creating Bbolt DB : %s", err)
+		return err
 	}
-	db.View(func(tx *bolt.Tx) error {
-		// Assume bucket exists and has keys
-		//b := tx.Bucket([]byte("State"))
-		// TODO check whether DB contains a containerid in the slice containers
+	defer db.Close()
+
+	containers, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Printf("Error getting running containers : %s", err)
+		return err
+	}
+
+	runningContainers := make([]string, len(containers))
+	for _, c := range containers {
+		runningContainers = append(runningContainers, c.ID)
+	}
+	fmt.Printf("%++v\n", runningContainers)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		// Assume bucket exists
+		bucket := tx.Bucket([]byte("State"))
+		cursor := bucket.Cursor()
+
+		for ip, id := cursor.First(); ip != nil; ip, id = cursor.Next() {
+			// fmt.Printf("key=%s, value=%s\n", ip, id)
+			stillExist := false
+			for _, cID := range runningContainers {
+				if strings.Compare(cID, fmt.Sprintf("%s", id)) == 0 {
+					stillExist = true
+				}
+			}
+			if stillExist {
+				log.Printf("IP %s still associated with container %s", ip, id)
+			} else {
+				err = bucket.Delete(ip)
+				if err != nil {
+					return err
+				}
+				log.Printf("IP %s removed from database", ip)
+			}
+		}
 		return nil
 	})
-	fmt.Printf("%v\n", containers)
+	return err
 }
 
 func main() {
 	flag.Parse()
 
 	go func() {
-
 		for {
+			log.Printf("DB cleaning started")
+			cleanDB()
 			// Wait for 10s.
 			time.Sleep(10 * time.Second)
-
-			log.Printf("DB cleaning started")
-
-			cleanDB()
-
 		}
-
 	}()
 
 	http.HandleFunc("/create/", provideChallengeBox)
